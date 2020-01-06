e0b34f321b2d693091fb51dddeae8b7dde92a392
Sakthivel.d <sakthivel.d@inxsasia.com>
2020-01-06 18:40:49 +0530

    1. working

--- app/src/main/java/com/sakthiveld/nio/MainActivity.java
+++ app/src/main/java/com/sakthiveld/nio/MainActivity.java
@@ -1,26 +1,29 @@
 package com.sakthiveld.nio;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.FileChannel;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.zip.Deflater;
 
 public class MainActivity extends AppCompatActivity {
-    SocketChannel socketChannel;
+
+    private static final String sHost = "uatstream.angelbroking.com";
+    private static final int iPort = 8444;
+    private static final String sStreamingRequest = "63=FT3.0|64=206|65=1|1=5$7=214599|1=5$7=214908|230=1";
+
+    SocketChannel socketChannel = null;
     SocketTask socketTask;
     RecyclerView recycler_view;
 
@@ -39,18 +42,69 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private ByteBuffer makeRequest(){
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+        byte[] send = new byte[1024];
+        Deflater compresser = new Deflater();
+        compresser.setInput("63=FT3.0|64=206|65=1|1=1$7=26000|230=1|".getBytes());
+        compresser.finish();
+        int prefixLength = compresser.deflate(send);
+        compresser.end();
+        byte start = 5;
+        String convertValue = Integer.toString(prefixLength);
+        String actualLength = getZeroString(convertValue.length()) + convertValue;
+        byte[] actualBytes = new byte[1024];
+        actualBytes[0] = start;
+
+        return ByteBuffer.wrap(actualBytes);
+    }
+
+    public static final int CASE_1 = 1;
+    public static final int CASE_2 = 2;
+    public static final int CASE_3 = 3;
+    public static final int CASE_4 = 4;
+    public static String getZeroString(int length) {
+        switch (length) {
+            case CASE_1:
+                return "0000";
+            case CASE_2:
+                return "000";
+            case CASE_3:
+                return "00";
+            case CASE_4:
+                return "0";
+        }
+        return "";
+    }
+
     private void createSocket() {
         socketTask =  new SocketTask();
-        socketTask.execute();
+        socketTask.execute(makeRequest());
     }
 
     private boolean connected() {
-//        if (socketChannel != null)
-//            return socketChannel.isConnected();
-//        else{
-//            return false;
-//        }
-        return true;
+        if (socketChannel != null)
+            return socketChannel.isConnected();
+        else{
+            return false;
+        }
+    }
+
+    private byte[] byteReturn(){
+        byte[] send = new byte[1024];
+        Deflater compresser = new Deflater();
+        compresser.setInput("HELLO".getBytes());
+        compresser.finish();
+        int prefixLength = compresser.deflate(send);
+        compresser.end();
+        byte start = 5;
+        String convertValue = Integer.toString(prefixLength);
+        String actualLength = getZeroString(convertValue.length()) + convertValue;
+        byte[] actualBytes = new byte[1024];
+        actualBytes[0] = start;
+
+        return actualBytes;
     }
 
     class SocketTask extends AsyncTask<ByteBuffer, String, Boolean> {
@@ -64,27 +118,28 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 if(connected()) {
-                    SocketChannel socketChannel = SocketChannel.open();
+                    Log.e("Connection Report", "connected");
+
+                    ByteBuffer byteBuffer1 = ByteBuffer.allocate(1024);
+                    byteBuffer1.put(byteReturn());
+                    socketChannel.write(byteBuffer1);
+
+                    socketChannel.write(byteBuffers);
+
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                    Log.e("response", String.valueOf(socketChannel.read(byteBuffer)));
+
+
+                } else {
+                    socketChannel = SocketChannel.open();
                     socketChannel.connect(new InetSocketAddress("uatstream.angelbroking.com", 8444));
                     socketChannel.configureBlocking(true);
-
-                    String newData = "63=FT3.0|64=206|65=1|7=15083$1=1|7=236$1=1|7=5900$1=1|7=16669$1=1|7=16675$1=1|7=317$1=1|7=10604$1=1|230=1";
-                    ByteBuffer bb = ByteBuffer.allocate(480);
-                    bb.put(newData.getBytes());
-                    socketChannel.write(bb);
-                    socketChannel.finishConnect();
-                    Log.e("request", bb.toString());
-
-                    ByteBuffer bbb = ByteBuffer.allocate(84);
-                    int result = socketChannel.read(bbb);
-
-                    Log.e("response", String.valueOf(result));
-                } else {
-                    SocketNewTask socketNewTask = new SocketNewTask();
-                    socketNewTask.execute();
+                    Log.e("Connection Report", "Not connected");
+                    socketTask.cancel(true);
+                    createSocket();
                 }
             } catch (IOException e) {
-                Toast.makeText(MainActivity.this, "error"+e.toString(), Toast.LENGTH_SHORT).show();
+                Log.e("Connection Report", e.toString());
             }
             return true;
         }
@@ -95,29 +150,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    class SocketNewTask extends AsyncTask<ByteBuffer, String, Boolean> {
-
-        @Override
-        protected void onPreExecute() {
-        }
-
-        @Override
-        protected Boolean doInBackground(ByteBuffer... byteBuffers) {
-            try {
-                SocketChannel socketChannel = SocketChannel.open();
-                socketChannel.connect(new InetSocketAddress("uatstream.angelbroking.com", 8444));
-                socketChannel.configureBlocking(true);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            super.onPostExecute(aBoolean);
-        }
-    }
 
 }
 
